c8b8241d027c2cfd97e97dd8fba98c543b7cb165
zanit-solution <zanltd.bd@gmail.com>
2025-02-11 16:54:19 +0600

    feat:create news page

---
+++ src/pages/news-category/[slug].jsx
@@ -0,0 +1,13 @@
+import CategoryAllNews from '@/components/category-news/news';
+import React from 'react';
+
+const Home = () => {
+
+    return (
+        <div className='container-custom'> 
+             <CategoryAllNews/>
+        </div>
+    );
+};
+
+export default Home;
