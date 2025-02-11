b98a408667c6d4a8c8fd338c239c8ba0c4f0c2b6
zanit-solution <zanltd.bd@gmail.com>
2025-02-11 12:34:20 +0600

    feat:layout setup

---
+++ src/components/layouts/index.jsx
@@ -0,0 +1,16 @@
+// import Footer from "./footer";
+// import { Navbar } from "./navbar"
+
+import {   Poppins } from "next/font/google";
+
+const poppins = Poppins({ weight:[ "100", "200","300","400","500", "600" ,"700","800","900",], subsets: ["latin"] });
+
+export const LayoutPageWrapper = ({ children }) => {
+    return (
+        <section className={`flex flex-col min-h-screen ${poppins.className}`}>
+            {/* <Navbar/> */}
+            <main className="flex-grow">{children}</main>
+            {/* <Footer/> */}
+        </section>
+    )
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
+import { LayoutPageWrapper } from "@/components/layouts";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <LayoutPageWrapper>
+      <Component {...pageProps} />
+    </LayoutPageWrapper>
+  );
 }
